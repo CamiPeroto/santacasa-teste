9ed18c24333e9e86225594b81a30802b65851b13
CamiPeroto <camilabuenoperoto@gmail.com>
2025-07-19 18:04:35 -0300

    feat: adicionando as imagens e os icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head><meta name="apple-mobile-web-app-title" content="SantaCasa" /></head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
