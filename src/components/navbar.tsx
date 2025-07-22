a8ff51a6d146b7eb39343b0fcef093803d146ac5
CamiPeroto <camilabuenoperoto@gmail.com>
2025-07-21 21:04:52 -0300

    fix: ajustando o posicionamento do icone menu no mobile

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import Link from 'next/link';
-import { useState } from 'react';
 import { Menu, X } from 'lucide-react';
 import Image from 'next/image';
-import { Button } from './ui/button';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Button } from './ui/button';
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
@@ -15,8 +15,8 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-nav shadow-md sticky top-0 z-50">
-      <div className="grid grid-cols-3 items-center px-4 md:px-16">
+    <nav className="bg-nav shadow-md sticky top-0 z-50 px-4 md:px-16">
+      <div className="flex items-center justify-between h-20">
         <div className="flex items-center">
           <Image
             src="/images/logo.png"
@@ -26,19 +26,16 @@ export default function Navbar() {
           />
         </div>
 
-        <div className="hidden md:flex justify-center gap-6">
+        <div className="hidden md:flex gap-6">
           <Link href="/" className="text-gray-700 font-bold hover:text-black">
-            Home
+            Início
           </Link>
-          <Link
-            href="/about"
-            className="text-gray-700 font-bold hover:text-black"
-          >
-            About
+          <Link href="/" className="text-gray-700 font-bold hover:text-black">
+            Sobre
           </Link>
         </div>
 
-        <div className="flex justify-end items-center gap-2">
+        <div className="flex items-center gap-2">
           <div className="hidden md:block">
             <Button size="lg" onClick={handleCreateAccount}>
               Criar Conta
@@ -54,15 +51,15 @@ export default function Navbar() {
       </div>
 
       {open && (
-        <div className="md:hidden flex flex-col gap-4 mt-2 px-4">
+        <div className="md:hidden flex flex-col gap-4 mt-2">
           <Link href="/" onClick={() => setOpen(false)}>
-            Home
+            Início
           </Link>
-          <Link href="/about" onClick={() => setOpen(false)}>
-            About
+          <Link href="/" onClick={() => setOpen(false)}>
+            Sobre
           </Link>
-          <Link href="/contact" onClick={() => setOpen(false)}>
-            Contact
+          <Link href="/" onClick={() => setOpen(false)}>
+            Contato
           </Link>
         </div>
       )}
